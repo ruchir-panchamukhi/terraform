449bd33af1371267df85316a1e3221301e49e2ad
ruchir-panchamukhi <ruchir.panchamukhi@Triconinfotech.com>
2024-11-19 10:13:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@ resource "aws_instance" "my_instance" {
   ami           = "ami-08c40ec9ead489470" # Amazon Linux 2 AMI (update to your region's AMI)
   instance_type = "t2.micro"
 
-  subnet_id              = aws_subnet.my_subnet.id
-  security_groups        = [aws_security_group.my_sg.name]
   associate_public_ip_address = true
 
   tags = {
